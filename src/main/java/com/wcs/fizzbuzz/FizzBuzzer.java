55b7cd98be8a76918be40af4207a7cb3bd1dff1a
Horváth Dániel <horvath.dani8909@gmail.com>
2015-02-28 12:37:29 +0100

    5 többszöröseit teljesíti a kód.

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -5,6 +5,8 @@ public class FizzBuzzer {
 	public String execute(int number) {
 		if(number % 5 == 0 && number % 3 == 0) {
 			return "fizzbuzz";
+		} else if(number % 5 == 0) {
+			return "buzz";
 		}
 		
 		return Integer.toString(number);
