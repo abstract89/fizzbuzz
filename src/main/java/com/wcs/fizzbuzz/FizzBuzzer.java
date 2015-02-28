71c407da820bb817f3ddb9bd7659427e05619b28
Horváth Dániel <horvath.dani8909@gmail.com>
2015-02-28 14:04:50 +0100

    5-öst tartalmazó számokra is kiírja hogy buzz

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -5,15 +5,14 @@ public class FizzBuzzer {
 	public String execute(int number) {
 		String stringNumber = Integer.toString(number);
 		String returnValue = "";
-		
-		if (number % 5 == 0) {
-			returnValue = "buzz";
-			if (number % 3 == 0) {
-				return   "fizz" + returnValue;
-			}
-		}
 
-		if (number % 3 == 0 || stringNumber.contains("3")) {
+		if (number % 5 == 0 && number % 3 == 0) {
+			return "fizzbuzz";
+		}
+		
+		if (number % 5 == 0 || stringNumber.contains("5")) {
+			returnValue = "buzz";
+		} else if (number % 3 == 0 || stringNumber.contains("3")) {
 			returnValue = "fizz";
 		}
 
--- src/test/java/com/wcs/fizzbuzz/FizzBuzzerTest.java
+++ src/test/java/com/wcs/fizzbuzz/FizzBuzzerTest.java
@@ -5,41 +5,46 @@ import static org.junit.Assert.*;
 
 public class FizzBuzzerTest {
 
-    private FizzBuzzer fizzBuzzer = new FizzBuzzer();
+	private FizzBuzzer fizzBuzzer = new FizzBuzzer();
 
-    @Test
-    public void executeShouldReturnFizzIfTheNumberIsDividableBy3() {
-        assertEquals("fizz", fizzBuzzer.execute(3));
-    }
+	@Test
+	public void executeShouldReturnFizzIfTheNumberIsDividableBy3() {
+		assertEquals("fizz", fizzBuzzer.execute(3));
+	}
 
-    @Test
-    public void executeShouldReturnBuzzIfTheNumberIsDividableBy5() {
-        assertEquals("buzz", fizzBuzzer.execute(5));
-        assertEquals("buzz", fizzBuzzer.execute(10));
-    }
+	@Test
+	public void executeShouldReturnBuzzIfTheNumberIsDividableBy5() {
+		assertEquals("buzz", fizzBuzzer.execute(5));
+		assertEquals("buzz", fizzBuzzer.execute(10));
+	}
 
-    @Test
-    public void executeShouldReturnBuzzIfTheNumberIsDividableBy15() {
-        assertEquals("fizzbuzz", fizzBuzzer.execute(15));
-        assertEquals("fizzbuzz", fizzBuzzer.execute(30));
-    }
+	@Test
+	public void executeShouldReturnBuzzIfTheNumberIsDividableBy15() {
+		assertEquals("fizzbuzz", fizzBuzzer.execute(15));
+		assertEquals("fizzbuzz", fizzBuzzer.execute(30));
+	}
 
-    @Test
-    public void executeShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled() {
-        assertEquals("1", fizzBuzzer.execute(1));
-        assertEquals("2", fizzBuzzer.execute(2));
-        assertEquals("4", fizzBuzzer.execute(4));
-//        assertEquals("7", fizzBuzzer.execute(7));
-    }
-	
+	@Test
+	public void executeShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled() {
+		assertEquals("1", fizzBuzzer.execute(1));
+		assertEquals("2", fizzBuzzer.execute(2));
+		assertEquals("4", fizzBuzzer.execute(4));
+		//assertEquals("7", fizzBuzzer.execute(7));
+	}
+
 	@Test
 	public void executeShouldReturnWizzIfTheNumberIsDividableBy7() {
 		assertEquals("wizz", fizzBuzzer.execute(21));
 		assertEquals("wizz", fizzBuzzer.execute(35));
 	}
-	
+
 	@Test
 	public void executeShouldRerturnWizzIfTheNumberContains3() {
 		assertEquals("fizz", fizzBuzzer.execute(43));
+	}
+
+	@Test
+	public void executeShouldRerturnWizzIfTheNumberContains5() {
+		assertEquals("buzz", fizzBuzzer.execute(54));
 	}
 }
