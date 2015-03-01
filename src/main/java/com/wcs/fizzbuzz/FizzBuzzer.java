580f3236e263c99603c8de70197a269492e25bf3
Horváth Dániel <horvath.dani8909@gmail.com>
2015-03-01 09:43:42 +0100

    Változó definíciók későbbre helyezése.

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -3,12 +3,12 @@ package com.wcs.fizzbuzz;
 public class FizzBuzzer {
 
 	public String execute(int number) {
-		String stringNumber = Integer.toString(number);
-		String returnValue = "";
-
 		if (number % 5 == 0 && number % 3 == 0) {
 			return "fizzbuzz";
 		}
+
+		String stringNumber = Integer.toString(number);
+		String returnValue = "";
 
 		if (number % 3 == 0 || stringNumber.contains("3")) {
 			returnValue += "fizz";
